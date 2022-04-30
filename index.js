19d114177083739e43145fb8f2e995dfe9b25548
Renan <renan.aragorn@gmail.com>
2022-04-29 23:13:18 -0300

    seconds countdown working, buttons responding

---
+++ index.js
@@ -0,0 +1,61 @@
+import { buttonPLay, minutesChoice, buttonPause, buttonStop, buttonSetTimer, buttonSoundStop, buttonSoundContinue, minutesDisplay, secondsDisplay } from './modules/elements.js'
+
+function chooseTime() {
+  minutesChoice = Number(prompt("Timer time?"))
+  if (minutesChoice == 0) {
+  } else {
+    minutesDisplay.textContent = minutesChoice
+  }
+}
+
+function countdown() {
+  setInterval(function() {
+    let secondsTimer = Number(secondsDisplay.textContent)
+    if (secondsTimer == 0) {
+      secondsTimer = 11
+    }
+    secondsDisplay.textContent = String(secondsTimer - 1).padStart(2, "0")
+    
+  }, 1000) 
+
+}
+
+
+
+buttonPLay.addEventListener('click', function() {
+  buttonPLay.classList.add("hide")
+  buttonPause.classList.remove("hide")
+  buttonStop.classList.remove("hide")
+  buttonSetTimer.classList.add("hide")
+  countdown()
+})
+
+buttonPause.addEventListener('click', function() {
+  buttonPLay.classList.remove("hide")
+  buttonPause.classList.add("hide")
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPLay.classList.remove("hide")
+  buttonPause.classList.add("hide")
+  buttonSetTimer.classList.remove("hide")
+  buttonStop.classList.add("hide")
+})
+
+buttonSetTimer.addEventListener('click', function() {
+  chooseTime()
+})
+
+
+
+buttonSoundStop.addEventListener('click', function() {
+  buttonSoundContinue.classList.remove("hide")
+  buttonSoundStop.classList.add("hide")
+  
+})
+
+buttonSoundContinue.addEventListener('click', function() {
+  buttonSoundContinue.classList.add("hide")
+  buttonSoundStop.classList.remove("hide")
+  
+})
---
+++ modules/elements.js
@@ -0,0 +1,22 @@
+
+const buttonPLay = document.querySelector(".play")
+const buttonPause = document.querySelector(".pause")
+const buttonStop = document.querySelector(".stop")
+const buttonSetTimer = document.querySelector(".set-timer")
+const buttonSoundContinue = document.querySelector(".sound-continue")
+const buttonSoundStop = document.querySelector(".sound-stop")
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
+let minutesChoice
+
+export {
+  buttonPLay,
+  buttonPause,
+  minutesChoice,
+  buttonStop,
+  buttonSetTimer,
+  buttonSoundContinue,
+  buttonSoundStop,
+  minutesDisplay,
+  secondsDisplay
+}
